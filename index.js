3e7a97294b342a083e82520b616c9e1287b2f4f9
TimCauss <t.caussignac@gmail.com>
2023-04-14 13:10:30 +0200

    feat : add the background, starting to create the Boundary object

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const c = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+class Boundary {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundary = new Boundary();
+//https://www.youtube.com/watch?v=5IMXpp3rohQ&ab_channel=ChrisCourses 13:56
